674bb5da6a7410e7b4a8bf99e0a118e1e2391870
Sérgio Oliveira <6653128+odhs@users.noreply.github.com>
2025-04-26 04:05:28 -0300

    feat: new custom 404 not-found error

---
+++ src/main/java/br/com/shdo/first_spring_app/infra/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.shdo.first_spring_app.infra;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public String handleNotFound() {
+        return "NOT FOUND";
+    }
+}
